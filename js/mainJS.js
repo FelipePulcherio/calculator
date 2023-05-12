bec2f41d880a77fbd988ae45de2d1167dc40f4ab
Felipe Pulcherio <felipe.pulcherio1@gmail.com>
2023-05-12 16:07:56 -0400

    Add condition to avoid two periods in number

--- js/mainJS.js
+++ js/mainJS.js
@@ -5,6 +5,7 @@ let thirdSlot = 0;
 function numberPressed() {
   const display2 = document.querySelector('.display2');
   if (display2.textContent.length >= 16) return;
+  if (/[.]/g.test(firstSlot) && this.textContent == ".") return;
   if (display2.textContent == 0) {
     firstSlot = this.textContent;
     display2.textContent = firstSlot;
