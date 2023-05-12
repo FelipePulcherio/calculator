77acca1aaad266a1047e4e92305e703dac1834c7
Felipe Pulcherio <felipe.pulcherio1@gmail.com>
2023-05-12 08:56:05 -0400

    Create function to update display value

--- js/mainJS.js
+++ js/mainJS.js
@@ -2,6 +2,18 @@ let firstSlot = 0;
 let secondSlot = 0;
 let thirdSlot = 0;
 
+function numberPressed() {
+  const display2 = document.querySelector('.display2');
+  if (display2.textContent.length >= 16) return;
+  if (display2.textContent == 0) {
+    firstSlot = this.textContent;
+    display2.textContent = firstSlot;
+  } else {
+    firstSlot = `${firstSlot + this.textContent}`;
+    display2.textContent = firstSlot;
+  }
+}
+
 const numbers = document.querySelectorAll('.number');
 numbers.forEach( (number) => number.addEventListener('click', numberPressed) );
 
